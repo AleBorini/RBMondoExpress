8c1f37611b9e62cca59e87af7e924ebe478ab56f
Alessandro Borini <63805010+AleBorini@users.noreply.github.com>
2025-11-02 10:37:05 +0000

    Initial commit - Track Server project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const mongoUri = "mongodb+srv://Follo:Follo@school-cluster-0.kxy8uw7.mongodb.net/?appName=School-Cluster-0"
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB successfully');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Failed to connect to MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+
